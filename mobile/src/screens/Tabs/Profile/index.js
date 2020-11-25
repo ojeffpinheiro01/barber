f225a830577ff8719ee85ef4ee0db274db8cfc88
ojeffpinheiro <jefersonpinheirodesouza@gmail.com>
2020-11-24 21:38:13 -0300

    melhorando tela de perfil

--- mobile/src/screens/Tabs/Profile/index.js
+++ mobile/src/screens/Tabs/Profile/index.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Button, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+
+import { UserContext } from '../../../contexts/UserContext';
 
 import Api from '../../../Api';
 
 import {
   Container,
+  AvatarIcon,
+  UserName,
+  UserEmail,
+  ButtonSignOut,
+  ButtonSignOutText
 } from './style';
 
 export default () => {
     const navigation = useNavigation();
+    const { state: user } = useContext(UserContext);
+    console.log(user)
     const handleLogoutClick = async () => {
         await Api.logout();
         navigation.reset({
@@ -19,8 +28,12 @@ export default () => {
 
     return(
         <Container>
-            <Text>Perfil</Text>
-            <Button title="SAIR" onPress={handleLogoutClick} />
+            <UserName>{user.name}</UserName>
+            <UserEmail>{user.email}</UserEmail>
+            <AvatarIcon source={{uri: user.avatar}} />
+            <ButtonSignOut onPress={handleLogoutClick}>
+                    <ButtonSignOutText>SAIR</ButtonSignOutText>
+                </ButtonSignOut>
         </Container>
     )
 }
--- mobile/src/screens/Tabs/Profile/style.js
+++ mobile/src/screens/Tabs/Profile/style.js
@@ -4,47 +4,34 @@ import styled from 'styled-components/native';
 export const Container = styled.SafeAreaView`
   flex: 1;
   background-color: #63C2D1;
-`;
-export const Scroller = styled.ScrollView`
-  flex: 1;
-  padding: 20px;
-`;
-export const HeaderArea = styled.View`
-  flex-direction: row;
-  justify-content: space-between;
   align-items: center;
+  justify-content: center;
 `;
-export const HeaderTitle = styled.Text`
-  width: 250px;
+
+export const AvatarIcon = styled.Image`
+  margin: 50px;
+  width: 240px;
+  height: 240px;
+  border-radius: 120px;
+`;
+export const UserName = styled.Text`
   font-size: 24px;
   font-weight: bold;
   color: #fff;
 `;
-export const SearchButton = styled.TouchableOpacity`
-  width: 26px;
-  height: 26px; 
-`;
-export const LocationArea = styled.View`
-  background: #4EADBE;
-  height: 60px;
-  border-radius: 30px;
-  flex-direction: row;
-  align-items: center;
-  padding: 0 20px;
-  margin-top: 30px;
-`;
-export const LocationInput = styled.TextInput`
-  flex: 1;
-  font-size: 16px;
+export const UserEmail = styled.Text`
+  font-size: 15px;
   color: #fff;
 `;
-export const LocationFinder = styled.TouchableOpacity`
-  width: 24px;
-  height: 24px;
+export const ButtonSignOut = styled.TouchableOpacity`
+  width: 80%;
+  height: 8%;
+  background-color: #268596;
+  border-radius: 30px;
+  justify-content: center;
+  align-items: center;
 `;
-export const LoadingIcon = styled.ActivityIndicator`
-  margin-top: 50px;
-`;
-export const ListArea = styled.View`
-  margin: 30px 0;
+export const ButtonSignOutText = styled.Text`
+    font-size: 18px;
+    color: #FFF;
 `;
